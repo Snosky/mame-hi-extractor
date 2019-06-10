fb341e1cdf098a163c45ae8851a52d5e13fb9694
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:47:02 +0200

    Update galaga

--- src/Extractor/Galaga.ts
+++ src/Extractor/Galaga.ts
@@ -1,24 +1,24 @@
-import AbstractExtractor from "../AbstractExtractor";
-import {join} from "path";
+import AbstractExtractor from '../AbstractExtractor';
+import {join} from 'path';
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'galaga',
+    hi: true
+})
 export default class Galaga extends AbstractExtractor {
-
-    constructor(filePath: string, fileName = 'galaga.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.output.default.push({
                 rank: i + 1,
-                score: this.buffer.slice(currentByte, 6).trim(0x24).decodeBCDLE(),
+                score: this.hi!.slice(currentByte, 6).trim(0x24).decodeBCDLE(),
                 name: ''
             });
             currentByte += 6;
         }
         for (let i = 0; i < 5; i++) {
-            this.output.default[i].name = this.asciiOffset(this.buffer.slice(currentByte, 3).buffer.toString(), 55);
+            this.output.default[i].name = this.asciiOffset(this.hi!.slice(currentByte, 3).buffer.toString(), 55);
             currentByte += 3;
         }
     }
