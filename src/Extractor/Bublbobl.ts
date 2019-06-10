019ff47c75b88eac61bfccbcd439578183547429
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:46:10 +0200

    Update bublbobl

--- src/Extractor/Bublbobl.ts
+++ src/Extractor/Bublbobl.ts
@@ -1,18 +1,19 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from 'path';
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'bublbobl',
+    hi: true
+})
 export default class Bublbobl extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'bublbobl.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract() {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntLE(currentByte, 3).toString(16)) * 10,
-                name: this.buffer.slice(currentByte + 4, 3).buffer.toString(),
+                score: parseInt(this.hi!.buffer.readIntLE(currentByte, 3).toString(16)) * 10,
+                name: this.hi!.slice(currentByte + 4, 3).buffer.toString(),
                 // TODO : Extras SCORE
             });
             currentByte += 7;
