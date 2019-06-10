47720f9602bad8daae878a984d0e2a759780e0c9
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:45:26 +0200

    Update btime

--- src/Extractor/Btime.ts
+++ src/Extractor/Btime.ts
@@ -1,24 +1,25 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from 'path';
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'btime',
+    hi: true
+})
 export default class Btime extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'btime.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract() {
         let currentByte = 3; // Skip TOP SCORE
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntLE(currentByte, 3).toString(16)),
+                score: parseInt(this.hi!.buffer.readIntLE(currentByte, 3).toString(16)),
                 name: ''
             });
             currentByte += 3;
         }
         currentByte += 3; // Skip Separator
         for (let i = 0; i < 5; i++) {
-            this.scores.default[i].name = this.asciiOffset(this.buffer.slice(currentByte, 3).buffer.toString(), 54); // TODO : Charset
+            this.scores.default[i].name = this.asciiOffset(this.hi!.slice(currentByte, 3).buffer.toString(), 54); // TODO : Charset
         }
     }
 }
