cf789f99c2d5e3bac09a9348330fa472f44d8650
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:47:34 +0200

    Update marble

--- src/Extractor/Marble.ts
+++ src/Extractor/Marble.ts
@@ -1,18 +1,19 @@
 import AbstractExtractor from "../AbstractExtractor";
 import {join} from "path";
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'marble',
+    hi: true,
+})
 export default class Marble extends AbstractExtractor {
-    public constructor(filePath: string) {
-        super(join(filePath, 'marble.hi'));
-    }
-
     extract(): any {
         let rank = 1;
-        for (let i = 0; i < this.buffer.buffer.length; i += 5) {
+        for (let i = 0; i < this.hi!.buffer.length; i += 5) {
             this.output.default.push({
                 rank: rank++,
-                score: this.buffer.buffer.readIntBE(i, 3),
-                name: this.buffer.slice(i + 3, 2).decodeBase40()
+                score: this.hi!.buffer.readIntBE(i, 3),
+                name: this.hi!.slice(i + 3, 2).decodeBase40()
             });
         }
     }
