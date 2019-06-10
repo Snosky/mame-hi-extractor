3f3cc04383c273eeee5e5a82978f7351959be066
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:41:28 +0200

    Update aburner

--- src/Extractor/Aburner.ts
+++ src/Extractor/Aburner.ts
@@ -1,21 +1,22 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from "path";
+import Extractor from '../Decorator/Extractor';
 
 /**
  * FIXME : Not Tested
  */
+@Extractor({
+    name: 'aburner',
+    hi: true
+})
 export default class Aburner extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'aburner.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
          let currentByte = 0;
          for (let i = 0; i < 20; i++) {
              this.scores.default.push({
                  rank: i + 1,
-                 score: parseInt(this.buffer.buffer.readIntBE(currentByte, 4).toString(16)),
-                 name: this.buffer.slice(currentByte + 4, 4).buffer.toString(),
+                 score: parseInt(this.hi!.buffer.readIntBE(currentByte, 4).toString(16)),
+                 name: this.hi!.slice(currentByte + 4, 4).buffer.toString(),
                  // TODO : Extras, HIT, MEDALS
              });
              currentByte += 14;
