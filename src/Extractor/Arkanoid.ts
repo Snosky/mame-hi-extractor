efb6bd51a3f5ba9903a0dc4bd31f3039c52b994f
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:42:52 +0200

    Update Arkanoid

--- src/Extractor/Arkanoid.ts
+++ src/Extractor/Arkanoid.ts
@@ -1,18 +1,19 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from "path";
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'arkanoid',
+    hi: true
+})
 export default class Arkanoid extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'arkanoid.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntBE(currentByte, 3).toString(16)) * 10,
-                name: this.buffer.slice(currentByte + 4, 3).buffer.toString()
+                score: parseInt(this.hi!.buffer.readIntBE(currentByte, 3).toString(16)) * 10,
+                name: this.hi!.slice(currentByte + 4, 3).buffer.toString()
             });
             currentByte += 7;
         }
