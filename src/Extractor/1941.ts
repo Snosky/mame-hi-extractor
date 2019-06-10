bda73e35d0783e411f05784f32245fe7b520071b
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-10 15:52:04 +0200

    1941 extractor

---
+++ src/Extractor/1941.ts
@@ -0,0 +1,21 @@
+import AbstractExtractor from '../AbstractExtractor';
+import {join} from 'path';
+
+export default class Extractor extends AbstractExtractor {
+    constructor(filePath: string, fileName = '1941.hi') {
+        super(join(filePath, fileName));
+    }
+
+    extract(): any {
+        let currentByte = 40;
+        for (let i = 0; i < 10; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.buffer.buffer.readIntBE(currentByte, 4).toString(16)),
+                name: this.buffer.slice(currentByte + 4, currentByte + 7).buffer.toString(),
+                // TODO : Extras data like GRADE
+            });
+            currentByte += 8;
+        }
+    }
+}
