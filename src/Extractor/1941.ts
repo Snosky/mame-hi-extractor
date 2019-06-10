548eb3d4a10838424ca3e83a99c22eb5ef2d98ca
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:40:41 +0200

    Update 1941

--- src/Extractor/1941.ts
+++ src/Extractor/1941.ts
@@ -1,18 +1,19 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from 'path';
+import Extractor from '../Decorator/Extractor';
 
-export default class Extractor extends AbstractExtractor {
-    constructor(filePath: string, fileName = '1941.hi') {
-        super(join(filePath, fileName));
-    }
-
+@Extractor({
+    name: '1941',
+    hi: true
+})
+export default class Extractor1941 extends AbstractExtractor {
     extract(): any {
         let currentByte = 40;
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntBE(currentByte, 4).toString(16)),
-                name: this.buffer.slice(currentByte + 4, 3).buffer.toString(),
+                score: parseInt(this.hi!.buffer.readIntBE(currentByte, 4).toString(16)),
+                name: this.hi!.slice(currentByte + 4, 3).buffer.toString(),
                 // TODO : Extras data like GRADE
             });
             currentByte += 8;
