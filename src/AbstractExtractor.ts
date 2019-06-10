de85d6ff22079a5a489a8f97359d3a76e438d9a0
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:38:29 +0200

    Rework AbstractExtractor for Extractor decorator

--- src/AbstractExtractor.ts
+++ src/AbstractExtractor.ts
@@ -1,14 +1,26 @@
 import {readFileSync} from "fs";
 import MHEBuffer from "./MHEBuffer";
+import {join} from 'path';
 
 export default abstract class AbstractExtractor {
+    private gameName = '';
+    private hasHi = false;
+    private nvramName = '';
 
-    protected buffer!: MHEBuffer;
+    protected hi?: MHEBuffer;
+    protected nvram?: MHEBuffer;
+
     protected output: Output = {default: []};
 
-    public constructor(file: string) {
-        this.buffer = new MHEBuffer(readFileSync(file));
+    public init(filePath: string) {
+        if (this.hasHi) {
+            this.hi = new MHEBuffer(readFileSync(join(filePath, 'hi', this.gameName + '.hi')));
+        }
+        if (this.nvramName) {
+            this.nvram = new MHEBuffer(readFileSync(join(filePath, 'nvram', this.gameName, this.nvramName)))
+        }
         this.extract();
+        return this;
     }
 
     abstract extract(): any;
