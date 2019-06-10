99aa18819631fb1ccc7d32b00ca17b4f992cfb1e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:43:36 +0200

    Update asteroid

--- src/Extractor/Asteroid.ts
+++ src/Extractor/Asteroid.ts
@@ -1,24 +1,25 @@
 import AbstractExtractor from '../AbstractExtractor';
-import {join} from "path";
+import {join} from 'path';
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'asteroid',
+    hi: true
+})
 export default class Asteroid extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'asteroid.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
         let currentBytes = 0;
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntLE(currentBytes, 2).toString(16)) * 10,
+                score: parseInt(this.hi!.buffer.readIntLE(currentBytes, 2).toString(16)) * 10,
                 name: '',
             });
             currentBytes += 2;
         }
         currentBytes += 3; //Separator
         for (let i = 0; i < 10; i++) {
-            this.scores.default[i].name = this.asciiOffset(this.buffer.slice(currentBytes, 3).buffer.toString(), 54);
+            this.scores.default[i].name = this.asciiOffset(this.hi!.slice(currentBytes, 3).buffer.toString(), 54);
             currentBytes += 3;
         }
     }
