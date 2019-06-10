220bedb7dd32b19e6934e693c8386d6e31dccef5
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:44:19 +0200

    Update bombjack

--- src/Extractor/Bombjack.ts
+++ src/Extractor/Bombjack.ts
@@ -1,17 +1,18 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from "path";
+import Extractor from '../Decorator/Extractor';
 
+@Extractor({
+    name: 'bombjack',
+    hi: true
+})
 export default class Bombjack extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'bombjack.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
         let currentByte = 16;
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                score: parseInt(this.buffer.buffer.readIntLE(currentByte, 4).toString(16)),
+                score: parseInt(this.hi!.buffer.readIntLE(currentByte, 4).toString(16)),
                 name: ''
             });
             currentByte += 4;
@@ -20,7 +21,7 @@ export default class Bombjack extends AbstractExtractor {
         currentByte += 10;
         for (let i = 0; i < 10; i++) {
             currentByte += 2; // Skip RANK
-            this.scores.default[i].name = this.buffer.slice(currentByte, 6).byteSkip(true).buffer.toString();
+            this.scores.default[i].name = this.hi!.slice(currentByte, 6).byteSkip(true).buffer.toString();
             currentByte += 8;
         }
     }
