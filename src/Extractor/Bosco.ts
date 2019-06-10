32efcec61e98e92f8aec6ead2a41cce58bd04786
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 00:44:52 +0200

    Update bosco

--- src/Extractor/Bosco.ts
+++ src/Extractor/Bosco.ts
@@ -1,26 +1,27 @@
 import AbstractExtractor from '../AbstractExtractor';
 import {join} from "path";
+import Extractor from '../Decorator/Extractor';
 
 /*
     TODO : Charsets
  */
+@Extractor({
+    name: 'bosco',
+    hi: true
+})
 export default class Bosco extends AbstractExtractor {
-    constructor(filePath: string, fileName = 'bosco.hi') {
-        super(join(filePath, fileName));
-    }
-
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                name: this.asciiOffset(this.buffer.slice(currentByte, 3).buffer.toString(), 55),
+                name: this.asciiOffset(this.hi!.slice(currentByte, 3).buffer.toString(), 55),
                 score: 0,
             });
             currentByte += 3;
         }
         for (let i = 0; i < 5; i++) {
-            this.scores.default[i].score = parseInt(this.buffer.buffer.readIntBE(currentByte, 4).toString(16));
+            this.scores.default[i].score = parseInt(this.hi!.buffer.readIntBE(currentByte, 4).toString(16));
             currentByte += 4;
         }
     }
