4641df1bd00c7583795ab588a65398d6ed82ab12
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 13:47:14 +0200

    Add defender

---
+++ src/Extractor/Defender.ts
@@ -0,0 +1,21 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'defender',
+    hi: false,
+    nvram: 'nvram'
+})
+export default class Defender extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 29; // Skip
+        for (let i = 0; i < 8; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: this.nvram!.slice(currentByte, 6).decodeBCD(),
+                name: this.nvram!.slice(currentByte + 6, 6).nibbleSkip(false).buffer.toString()
+            });
+            currentByte += 12;
+        }
+    }
+}
