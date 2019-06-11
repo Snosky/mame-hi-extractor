19cd305adfea4596fe6f348a5537e78a82ffbce5
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 11:41:09 +0200

    hasHi is true by default

--- src/Decorator/Extractor.ts
+++ src/Decorator/Extractor.ts
@@ -2,7 +2,7 @@ export default function Extractor(options: ExtractorOptions) {
     return <T extends {new(...args:any[]):{}}>(constructor:T) => {
         return class extends constructor {
             gameName = options.name;
-            hasHi = options.hi || false;
+            hasHi = options.hi === undefined ? true : options.hi;
             nvramName = options.nvram || false;
         }
     }
