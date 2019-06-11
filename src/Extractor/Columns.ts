11571cdef5c14dedd4b091c95a17280397b2f3a5
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 11:39:51 +0200

    Add columns

---
+++ src/Extractor/Columns.ts
@@ -0,0 +1,21 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'columns',
+})
+export default class Columns extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 9; i++) {
+            currentByte++; // Skip header
+            this.scores.default.push({
+                rank: i + 1,
+                name: this.hi!.slice(currentByte, 3).buffer.toString(),
+                score: this.hi!.buffer.readIntBE(currentByte + 4, 4),
+                // TODO : Extras
+            });
+            currentByte += 15;
+        }
+    }
+}
