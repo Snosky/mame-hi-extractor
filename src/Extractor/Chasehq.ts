ab394fc61e0c9c3a92f986a582e43792ea330077
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-11 11:30:14 +0200

    add chasehq

---
+++ src/Extractor/Chasehq.ts
@@ -0,0 +1,18 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'chasehq'
+})
+export default class Chasehq extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 10; i++) {
+            let byte = i * 16 + 6; // Each line is 16 bytes, skip 6 first bytes because it's useless
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.buffer.readIntBE(byte, 4).toString(16)),
+                name: this.hi!.slice(byte + 6, 3).buffer.toString()
+            });
+        }
+    }
+}
