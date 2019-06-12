287a86b3cddb49761071e2cb1d478f7a1d6a9da1
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-12 13:54:56 +0200

    Add hyperspt

---
+++ src/Extractor/Hyperspt.ts
@@ -0,0 +1,75 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'hyperspt',
+    hi: false,
+    nvram: 'nvram'
+})
+export default class Hyperspt extends AbstractExtractor {
+    protected extraIds = [
+        'tripleJump',
+        'shooting',
+        'pole',
+        'freeStyle',
+        'weight',
+        'archery',
+        'horse',
+    ];
+
+    extract(): any {
+        let currentByte = 0; // Jump first 1024 Bytes
+
+        this.output.extras = {};
+        for (const extra of this.extraIds) {
+            this.output.extras[extra] = [];
+            for (let i = 0; i < 4; i++) { // All extras hiscores are by group of 4
+                this.output.extras[extra].push({
+                    rank: i + 1,
+                    score: this.formatScore(this.nvram!.slice(currentByte,5).decodeBCD(), extra),
+                    name: this.asciiOffset(this.nvram!.slice(currentByte + 5, 3).buffer.toString(), 48),
+                    scoreSuffix: this.getScoreSuffix(extra)
+                });
+                currentByte += 8;
+            }
+        }
+        currentByte += 6; // Skip a spacing (6B)
+        this.output.extras['todays'] = [];
+        for (let i = 0; i < 3; i++) {
+            this.output.extras['todays'].push({
+                rank: i + 1,
+                score: parseInt(this.nvram!.buffer.readIntBE(currentByte, 3).toString(16)) * 10,
+                name: this.asciiOffset(this.nvram!.slice(currentByte + 3, 3).buffer.toString(), 48)
+            });
+            currentByte += 6;
+        }
+        for (let i = 0; i < 20; i++) { // Next 160 groups of 5 bytes are default scores
+            this.output.default.push({
+                rank: i + 1,
+                score: parseInt(this.nvram!.buffer.readIntBE(currentByte, 3).toString(16)) * 10,
+                name: this.asciiOffset(this.nvram!.slice(currentByte + 3, 3).buffer.toString(), 48)
+            });
+            currentByte += 6;
+        }
+    }
+
+    protected formatScore(score: number, id: string) {
+        if (['freeStyle', 'tripleJump', 'pole', 'freeStyle', 'horse'].indexOf(id) >= 0) {
+            return score / 100;
+        } else if (id === 'weight') {
+            return score / 10;
+        }
+        return score;
+    }
+
+    protected getScoreSuffix(id: string) {
+        if (id === 'freeStyle') {
+            return 'sec';
+        } else if (id === 'weight') {
+            return 'kg'
+        } else if (id === 'pole' || id === 'tripleJump') {
+            return 'm'
+        }
+        return '';
+    }
+}
