eeaa6ebf4a40684075ae7ddb6e878fb6230f5282
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-12 13:58:37 +0200

    Add Insectx

---
+++ src/Extractor/Insectx.ts
@@ -0,0 +1,17 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'insectx'
+})
+export default class Insectx extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 10; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(i * 8, 3).readIntBE().toString(16)) * 100,
+                name: this.hi!.slice(i * 8 + 4, 3).buffer.toString()
+            })
+        }
+    }
+}
