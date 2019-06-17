2c241000105b0f1b4f34c38454b4600a6ccd1f91
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-17 16:58:59 +0200

    Add mario

---
+++ src/Extractor/Mario.ts
@@ -0,0 +1,17 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'mario'
+})
+export default class Mario extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 5; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(i * 34 + 7, 6).nibbleSkip(false).readIntBE().toString(16)),
+                name: this.asciiOffset(this.hi!.slice(i * 34 + 15, 3).buffer.toString(), 48)
+            })
+        }
+    }
+}
