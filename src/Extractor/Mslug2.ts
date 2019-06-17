22bc8d61f9f03659a1f6c65a6e215aadd5a1e382
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-17 17:50:25 +0200

    Add mslug

---
+++ src/Extractor/Mslug2.ts
@@ -0,0 +1,20 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'mslug2'
+})
+export default class Mslug2 extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(currentByte, 4).readIntBE().toString(16)),
+                name: this.hi!.slice(currentByte + 4, 3).buffer.toString()
+            });
+            currentByte += 10;
+        }
+
+    }
+}
