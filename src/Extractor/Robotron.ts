6b6ee84ac86a55ec8c757754230459db1965002e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-18 17:47:27 +0200

    Add robotron

---
+++ src/Extractor/Robotron.ts
@@ -0,0 +1,29 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'robotron',
+    hi: false,
+    nvram: 'nvram'
+})
+export default class Robotron extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 306;
+
+        this.scores.default.push({
+            rank: 1,
+            name: this.nvram!.slice(currentByte, 6).nibbleSkip().buffer.toString(), // TODO : Charset
+            score: parseInt(this.nvram!.slice(currentByte + 47, 7).nibbleSkip().readIntBE().toString(16))
+        });
+        currentByte += 54;
+
+        for (let i = 0; i < 37; i++) {
+            this.scores.default.push({
+                rank: i + 2,
+                name: this.nvram!.slice(currentByte, 6).nibbleSkip().buffer.toString(), // TODO : Charset
+                score: parseInt(this.nvram!.slice(currentByte + 7, 7).nibbleSkip().readIntBE().toString(16))
+            });
+            currentByte += 14;
+        }
+    }
+}
