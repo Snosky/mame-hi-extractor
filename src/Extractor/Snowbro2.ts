a2128dfbaf63b980ee1a6fc250f2f6575eaba40a
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-18 18:11:28 +0200

    Add snowbro2

---
+++ src/Extractor/Snowbro2.ts
@@ -0,0 +1,17 @@
+import Extractor from "../Decorator/Extractor";
+import AbstractExtractor from "../AbstractExtractor";
+
+@Extractor({
+    name: 'snowbro2'
+})
+export default class Snowbro2 extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 5; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(i * 4, 4).readIntBE().toString(16)),
+                name: this.hi!.slice(i * 4 + 20 + 1, 3).buffer.toString()
+            });
+        }
+    }
+}
