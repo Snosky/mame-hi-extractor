3f3835bdd4a4cc91eb6e8f6911d7ea5c23657ca9
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-18 10:48:06 +0200

    Add mwalk

---
+++ src/Extractor/Mwalk.ts
@@ -0,0 +1,17 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'mwalk'
+})
+export default class Mwalk extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 10; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(i * 8, 4).readIntBE().toString(16)),
+                name: this.hi!.slice(i * 8 + 5, 3).buffer.toString()
+            });
+        }
+    }
+}
