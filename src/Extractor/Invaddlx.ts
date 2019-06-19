9d11d71283a5e7d80de70602ece887778b96f0e9
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:09:05 +0200

    ADd charset to invaddlx

--- src/Extractor/Invaddlx.ts
+++ src/Extractor/Invaddlx.ts
@@ -5,11 +5,16 @@ import Extractor from "../Decorator/Extractor";
     name: 'invaddlx'
 })
 export default class Invaddlx extends AbstractExtractor {
+    protected charset = {
+        0x1A: '.',
+        0x1B: ' '
+    };
+
     extract(): any {
         this.scores.default.push({
             rank: 1,
             score: parseInt(this.hi!.slice(0, 2).readIntLE().toString(16)) * 10,
-            name: this.asciiOffset(this.hi!.slice(2, 10).buffer.toString(), 65), // TODO : Charset
+            name: this.hi!.slice(2, 10).toString(this.charset, 65)
         });
     }
 }
