ba6b329f2cf4e3fab018c416aaa60d3038484484
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 14:40:10 +0200

    Add charset to bosco

--- src/Extractor/Bosco.ts
+++ src/Extractor/Bosco.ts
@@ -1,20 +1,22 @@
 import AbstractExtractor from '../AbstractExtractor';
 import Extractor from '../Decorator/Extractor';
 
-/*
-    TODO : Charsets
- */
 @Extractor({
     name: 'bosco',
     hi: true
 })
 export default class Bosco extends AbstractExtractor {
+    protected charset = {
+        0x34: '.',
+        0x24: ' '
+    };
+
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                name: this.asciiOffset(this.hi!.slice(currentByte, 3).buffer.toString(), 55),
+                name: this.hi!.slice(currentByte, 3).toString(this.charset, 55),
                 score: 0,
             });
             currentByte += 3;
