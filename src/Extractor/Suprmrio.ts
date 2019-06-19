1114dbf95b55c4a756d488aa8982119018c12520
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 15:08:02 +0200

    Add suprmrio

---
+++ src/Extractor/Suprmrio.ts
@@ -0,0 +1,27 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+// TODO : Not tested
+@Extractor({
+    name: 'suprmrio'
+})
+export default class Suprmrio extends AbstractExtractor {
+    protected charset = {
+        0x24: ' ',
+        0x28: '-',
+        0x2B: '!',
+        0xAF: '.',
+        0xFA: '\\',
+    };
+
+    extract(): any {
+        for (let i = 0; i < 10; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                name: this.hi!.slice(29 + i * 3, 3).toString(this.charset, 55),
+                score: parseInt(this.hi!.slice(69 + i * 7, 7).nibbleSkip().readIntBE().toString(16))
+            });
+        }
+
+    }
+}
