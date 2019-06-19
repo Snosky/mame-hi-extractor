fc923fe1d18dded018d7ff43cc24f7491816cfbc
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:47:48 +0200

    Add charset to robotron

--- src/Extractor/Robotron.ts
+++ src/Extractor/Robotron.ts
@@ -7,12 +7,16 @@ import Extractor from "../Decorator/Extractor";
     nvram: 'nvram'
 })
 export default class Robotron extends AbstractExtractor {
+    protected charset = {
+        0x3A: ' '
+    };
+
     extract(): any {
         let currentByte = 306;
 
         this.scores.default.push({
             rank: 1,
-            name: this.nvram!.slice(currentByte, 6).nibbleSkip().buffer.toString(), // TODO : Charset
+            name: this.nvram!.slice(currentByte, 6).nibbleSkip().toString(this.charset),
             score: parseInt(this.nvram!.slice(currentByte + 47, 7).nibbleSkip().readIntBE().toString(16))
         });
         currentByte += 54;
@@ -20,7 +24,7 @@ export default class Robotron extends AbstractExtractor {
         for (let i = 0; i < 37; i++) {
             this.scores.default.push({
                 rank: i + 2,
-                name: this.nvram!.slice(currentByte, 6).nibbleSkip().buffer.toString(), // TODO : Charset
+                name: this.nvram!.slice(currentByte, 6).nibbleSkip().toString(this.charset),
                 score: parseInt(this.nvram!.slice(currentByte + 7, 7).nibbleSkip().readIntBE().toString(16))
             });
             currentByte += 14;
