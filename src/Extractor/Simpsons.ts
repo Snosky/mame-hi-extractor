5b8adcbcd695eda3a97064f526451d7f4bc03dcb
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:56:47 +0200

    Add charset to simpsons

--- src/Extractor/Simpsons.ts
+++ src/Extractor/Simpsons.ts
@@ -5,11 +5,23 @@ import Extractor from "../Decorator/Extractor";
     name: 'simpsons'
 })
 export default class Simpsons extends AbstractExtractor {
+    protected charset = {
+        0x40: ' ',
+        0x5B: '.',
+        0x5C: '-',
+        0x5D: '?',
+        0x5E: '!',
+        0x5F: '&',
+        0x60: '@',
+        0x61: '&black-star;',
+        0x62: '$',
+    };
+
     extract(): any {
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                name: this.hi!.slice(i * 8, 3).buffer.toString(), // TODO :Charset
+                name: this.hi!.slice(i * 8, 3).toString(this.charset),
                 score: parseInt(this.hi!.slice(i * 8 + 3, 2).readIntBE().toString(16))
             });
         }
