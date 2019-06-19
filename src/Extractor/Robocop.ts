7434c1e1b1af96b6ebbb0aa6c6fa5275734fba42
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:46:56 +0200

    Add charset to robocop

--- src/Extractor/Robocop.ts
+++ src/Extractor/Robocop.ts
@@ -6,11 +6,15 @@ import Extractor from "../Decorator/Extractor";
     name: 'robocop'
 })
 export default class Robocop extends AbstractExtractor {
+    protected charset  = {
+        0x00: ''
+    };
+
     extract(): any {
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                name: this.hi!.slice(i * 16, 8).buffer.toString(), // TODO: Charset
+                name: this.hi!.slice(i * 16, 8).toString(this.charset),
                 score: parseInt(this.hi!.slice(i * 16 + 8, 4).readIntBE().toString(16))
             });
         }
