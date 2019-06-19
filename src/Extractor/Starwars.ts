e25fc53a08fa48b5d9a1cb7e1256f08e68417813
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 15:02:35 +0200

    Add starwars

---
+++ src/Extractor/Starwars.ts
@@ -0,0 +1,23 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'starwars',
+    hi: false,
+    nvram: 'x2212'
+})
+export default class Starwars extends AbstractExtractor {
+    protected charset = {
+        0x00: ' '
+    };
+
+    extract(): any {
+        for (let i = 0; i < 3; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.nvram!.slice(8 + i * 8, 8).nibbleSkip().readIntBE().toString(16)),
+                name: this.nvram!.slice(32 + i * 6, 6).nibbleSkip().toString(this.charset, 64)
+            });
+        }
+    }
+}
