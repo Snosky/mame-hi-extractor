eb454351ee70e4504681e975ea43637c3d5592a0
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:10:58 +0200

    Add charset to jackal

--- src/Extractor/Jackal.ts
+++ src/Extractor/Jackal.ts
@@ -5,12 +5,20 @@ import Extractor from "../Decorator/Extractor";
     name: 'jackal'
 })
 export default class Jackal extends AbstractExtractor {
+    protected charset = {
+        0x0C: '-',
+        0x0D: '.',
+        0x0E: ':',
+        0x0F: '!',
+        0x10: ' '
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.slice(i * 8, 4).readIntBE().toString(16)),
-                name: this.asciiOffset(this.hi!.slice(i * 8 + 4, 3).buffer.toString(), 48) // TODO : Charset
+                name:this.hi!.slice(i * 8 + 4, 3).toString(this.charset, 48)
             })
         }
     }
