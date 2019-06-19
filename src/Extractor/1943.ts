dfbe987ec5a09d61c857cafc7ab312da763b26e2
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 15:51:53 +0200

    Add charset to 1943

--- src/Extractor/1943.ts
+++ src/Extractor/1943.ts
@@ -5,13 +5,22 @@ import Extractor from "../Decorator/Extractor";
     name: '1943',
 })
 export default class Extractor1943 extends AbstractExtractor {
+    protected charset = {
+        0x24: ' ',
+        0x2B: '.',
+        0x2C: ':',
+        0x3A: '&',
+        0x62: '&black-heart;',
+        0x64: '&black-star;', // TODO : Special char
+        0x66: '!'
+    };
+
     extract(): any {
         for (let i = 0; i < 6; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(i * 16, 8).decodeBCD(),
                 name: this.asciiOffset(this.hi!.slice(i * 16 + 8, 3).buffer.toString(), 55)
-                // TODO : Charset
             })
         }
     }
