1aa594dd22a35876b7997fdadbbf4ad40a9eb435
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:52:39 +0200

    ADd charset to scontra

--- src/Extractor/Scontra.ts
+++ src/Extractor/Scontra.ts
@@ -5,11 +5,20 @@ import Extractor from "../Decorator/Extractor";
     name: 'scontra'
 })
 export default class Scontra extends AbstractExtractor {
+    protected charset = {
+        0x0B: ',',
+        0x0C: '-',
+        0x0D: '.',
+        0x0E: '?',
+        0x0F: '!',
+        0x10: ' ',
+    };
+
     extract(): any {
         for (let i = 0; i < 10; i++) {
             this.scores.default.push({
                 rank: i + 1,
-                name: this.asciiOffset(this.hi!.slice(i * 8, 3).buffer.toString(), 48), // TODO : Charset
+                name: this.hi!.slice(i * 8, 3).toString(this.charset, 48),
                 score: parseInt(this.hi!.slice(i * 8 + 4, 4).readIntBE().toString(16))
             });
         }
