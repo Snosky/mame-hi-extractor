83f627dea3edd57ed58914ea8aacd0cf3f363539
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:02:34 +0200

    ADd charset to dkongjr

--- src/Extractor/Dkongjr.ts
+++ src/Extractor/Dkongjr.ts
@@ -5,6 +5,12 @@ import Extractor from "../Decorator/Extractor";
     name: 'dkongjr'
 })
 export default class Dkongjr extends AbstractExtractor {
+    protected charset = {
+        0x10: ' ',
+        0x2B: '.',
+        0x2C: '-'
+    };
+
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i< 5; i++) {
@@ -12,7 +18,7 @@ export default class Dkongjr extends AbstractExtractor {
             this.scores.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(currentByte, 6).decodeBCD(),
-                name: this.asciiOffset(this.hi!.slice(currentByte + 12, 3).buffer.toString(), 48) // TODO : Charset
+                name: this.hi!.slice(currentByte + 12, 3).toString(this.charset, 48)
             });
             currentByte += 27;
         }
