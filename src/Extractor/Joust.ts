a0ccc9bdd09609f9388b461531383d0140872355
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:13:10 +0200

    Add charset for joust

--- src/Extractor/Joust.ts
+++ src/Extractor/Joust.ts
@@ -7,21 +7,25 @@ import Extractor from "../Decorator/Extractor";
     nvram: 'nvram'
 })
 export default class Joust extends AbstractExtractor {
+    protected charset = {
+        0x0A: ' ',
+        0x32: ':'
+    };
+
     extract(): any {
         // Use only nvram, because hi is a duplicate of extra score 'Daily'
 
         let currentByte = 318; // Skip debug values
-        // TODO : Charset
         this.output.default.push({
             rank: 1,
-            name: this.asciiOffset(this.nvram!.slice(currentByte, 40).nibbleSkip(false).buffer.toString(), 54),
+            name: this.nvram!.slice(currentByte, 40).nibbleSkip(false).toString(this.charset, 54),
             score: parseInt(this.nvram!.slice(currentByte + 47, 7).nibbleSkip(false).readIntBE().toString(16))
         });
         currentByte += 54;
         for (let i = 0; i < 40; i++) {
             this.output.default.push({
                 rank: i + 2,
-                name: this.asciiOffset(this.nvram!.slice(currentByte, 6).nibbleSkip(false).buffer.toString(), 54),
+                name: this.nvram!.slice(currentByte, 6).nibbleSkip(false).toString(this.charset, 54),
                 score: parseInt(this.nvram!.slice(currentByte + 7, 7).nibbleSkip(false).readIntBE().toString(16))
             });
             currentByte += 14;
@@ -31,7 +35,7 @@ export default class Joust extends AbstractExtractor {
         for (let i = 0; i < 6; i++) {
             this.scores.extras.daily.push({
                 rank: i + 1,
-                name: this.asciiOffset(this.nvram!.slice(currentByte, 6).nibbleSkip(false).buffer.toString(), 54),
+                name: this.nvram!.slice(currentByte, 6).nibbleSkip(false).toString(this.charset, 54),
                 score: parseInt(this.nvram!.slice(currentByte + 7, 7).nibbleSkip(false).readIntBE().toString(16))
             });
             currentByte += 14
