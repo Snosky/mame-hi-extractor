1af70cc458558edb46448d4c18943f5519fbdbda
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:37:04 +0200

    Use charset

--- src/Extractor/Kungfum.ts
+++ src/Extractor/Kungfum.ts
@@ -5,12 +5,18 @@ import Extractor from "../Decorator/Extractor";
     name: 'kungfum'
 })
 export default class Kungfum extends AbstractExtractor {
+    protected charset = {
+        0x20: ' ',
+        0x2E: '.',
+        0x5B: '.'
+    };
+
     extract(): any {
         for (let i = 19; i >= 0; i--) {
             this.scores.default.push({
                 rank: Math.abs(i - 20),
                 score: parseInt(this.hi!.slice(i * 6, 3).readIntBE().toString(16)) * 10,
-                name: this.hi!.slice(i * 6 + 3, 3).toStringLE() // TODO : Charset
+                name: this.hi!.slice(i * 6 + 3, 3).toString(this.charset)
             })
         }
 
