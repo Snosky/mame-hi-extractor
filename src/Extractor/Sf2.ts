03a7753fb080997388db50410bc42f9c8b3ef40f
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:53:30 +0200

    Add charset to sf2

--- src/Extractor/Sf2.ts
+++ src/Extractor/Sf2.ts
@@ -5,12 +5,17 @@ import Extractor from "../Decorator/Extractor";
     name: 'sf2'
 })
 export default class Sf2 extends AbstractExtractor {
+    protected charset = {
+        0x20: ' ',
+        0x2E: '.'
+    };
+
     extract(): any {
         for (let i = 0; i < 6; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.slice(i * 8, 4).readIntBE().toString(16)),
-                name: this.hi!.slice(i * 8 + 4, 3).buffer.toString() // TODO : charset
+                name: this.hi!.slice(i * 8 + 4, 3).toString(this.charset)
             });
         }
     }
