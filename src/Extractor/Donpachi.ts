bb1fba714eca7625fb15d32969ec08335d3b3c97
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:04:18 +0200

    Add charset to donpachi

--- src/Extractor/Donpachi.ts
+++ src/Extractor/Donpachi.ts
@@ -5,13 +5,17 @@ import Extractor from "../Decorator/Extractor";
     name: 'donpachi'
 })
 export default class Donpachi extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x38: '.'
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.buffer.readIntBE(4 + (i * 4), 4).toString(16)),
-                name: this.asciiOffset(this.hi!.slice(24 + (i * 6), 6).byteSkip(false)
-                    .byteMap(byte => byte / 2).buffer.toString(), -9), // TODO : Charset
+                name: this.hi!.slice(24 + (i * 6), 6).byteSkip(false).byteMap(byte => byte / 2).toString(this.charset, -9)
                 // TODO : Extras
             })
         }
