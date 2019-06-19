f23b088791ed140d619bfb0b190a66bcfdd440cf
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 15:22:53 +0200

    Add tutankhm

---
+++ src/Extractor/Tutankhm.ts
@@ -0,0 +1,17 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+// TODO : Not tested
+@Extractor({
+    name: 'tutankhm'
+})
+export default class Tutankhm extends AbstractExtractor {
+    extract(): any {
+        for (let i = 0; i < 7; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: parseInt(this.hi!.slice(i * 6, 3).readIntBE().toString(16)),
+                name: this.hi!.slice(i * 6 + 3, 3).toString()
+            });
+        }
+    }
+}
