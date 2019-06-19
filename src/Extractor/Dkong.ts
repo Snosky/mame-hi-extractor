17b2439f078dbedc5a6312e4110ef579a9b2357c
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:00:04 +0200

    Add charset to dkong

--- src/Extractor/Dkong.ts
+++ src/Extractor/Dkong.ts
@@ -5,12 +5,18 @@ import Extractor from "../Decorator/Extractor";
     name: 'dkong'
 })
 export default class Dkong extends AbstractExtractor {
+    protected charset = {
+        0x10: ' ',
+        0x2B: '.',
+        0x2C: '-'
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.buffer.readIntLE(34 * i + 29, 3).toString(16)),
-                name: this.asciiOffset(this.hi!.slice(34 * i + 15, 3).buffer.toString(), 48) // TODO : Charset
+                name: this.hi!.slice(34 * i + 15, 3).toString(this.charset, 48)
             })
         }
     }
