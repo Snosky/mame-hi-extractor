faac40a44b739a696e28d52d126776382c6c5633
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:40:44 +0200

    Add charset to pacland

--- src/Extractor/Pacland.ts
+++ src/Extractor/Pacland.ts
@@ -6,12 +6,19 @@ import Extractor from "../Decorator/Extractor";
     name: 'pacland'
 })
 export default class Pacland extends AbstractExtractor {
+    protected charset = {
+        0xE4: '&crescent-moon;', // TODO : Special char
+        0xE5: '&headset;',
+        0xE6: '.',
+        0xE7: ' '
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.slice(i * 16, 4).readIntBE().toString(16)),
-                name: this.hi!.slice(i * 16 + 5, 5).buffer.toString() // TODO : Charset
+                name: this.hi!.slice(i * 16 + 5, 5).toString(this.charset)
             });
         }
     }
