a573f9432ea103d0fa0c29d6d1c154262ee07dc1
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:39:02 +0200

    Add charset to mappy

--- src/Extractor/Mappy.ts
+++ src/Extractor/Mappy.ts
@@ -5,12 +5,17 @@ import Extractor from "../Decorator/Extractor";
     name: 'mappy'
 })
 export default class Mappy extends AbstractExtractor {
+    protected charset = {
+        0x5F: '.'
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.slice(i * 8, 3).readIntBE().toString(16)) * 10,
-                name: this.hi!.slice(i * 8 + 5, 3).buffer.toString() // TODO : Extras and charset
+                name: this.hi!.slice(i * 8 + 5, 3).toString(this.charset)
+                // TODO : Extras
             })
         }
     }
