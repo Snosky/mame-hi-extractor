6686e9430622d3ce1f58d9e5b0518e096e203d86
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:44:49 +0200

    Add charset to pengo

--- src/Extractor/Pengo.ts
+++ src/Extractor/Pengo.ts
@@ -6,12 +6,16 @@ import Extractor from "../Decorator/Extractor";
     name: 'pengo'
 })
 export default class Pengo extends AbstractExtractor {
+    protected charset = {
+        0x5B: '.'
+    };
+
     extract(): any {
         for (let i = 4; i >= 0; i--) {
             this.scores.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(i * 6, 2).readIntLE() * 10,
-                name: this.hi!.slice(i * 6 + 3, 3).buffer.toString() // TODO : Buffer
+                name: this.hi!.slice(i * 6 + 3, 3).toString(this.charset)
             });
         }
     }
