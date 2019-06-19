839a8c5d6d80e735d2e0d89a2d357cef91ba01b7
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 16:51:10 +0200

    Add charset to rtype

--- src/Extractor/Rtype.ts
+++ src/Extractor/Rtype.ts
@@ -21,7 +21,7 @@ export default class Rtype extends AbstractExtractor {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.slice(currentByte, 3).byteSwap(2).readIntBE().toString(16)), // TODO : byteSwap will not work with an odd number of bytes
-                name: this.hi!.slice(currentByte + 3, 7).buffer.toString() // TODO : Charset
+                name: this.hi!.slice(currentByte + 3, 7).buffer.toString(this.charset)
             });
             currentByte += 11;
         }
