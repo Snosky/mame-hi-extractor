c30d7877fa0ab9d3e90595479341e8b903212955
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-19 15:56:31 +0200

    Add charset to ddragon

--- src/Extractor/Ddragon.ts
+++ src/Extractor/Ddragon.ts
@@ -5,12 +5,17 @@ import Extractor from "../Decorator/Extractor";
     name: 'ddragon'
 })
 export default class Ddragon extends AbstractExtractor {
+    protected charset = {
+        0x3B: '.',
+        0x3C: ' '
+    };
+
     extract(): any {
         for (let i = 0; i < 5; i++) {
             this.scores.default.push({
                 rank: i + 1,
                 score: parseInt(this.hi!.buffer.readIntBE(i * 6, 3).toString(16)),
-                name: this.asciiOffset(this.hi!.slice(i * 6 + 3, 3).buffer.toString(), 32) // TODO : charset
+                name: this.hi!.slice(i * 6 + 3, 3).toString(this.charset, 32)
             });
         }
     }
