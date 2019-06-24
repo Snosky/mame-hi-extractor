1f8d4e51ed8a235ba7ba639254a7979d3f133eb0
Tristan Payen <tristan.payen@infopro-digital.com>
2019-06-24 17:57:33 +0200

    Main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import Marble from "./Extractor/Marble";
-import Trackfld from "./Extractor/Trackfld";
-import Galaga from "./Extractor/Galaga";
+import extractors from './Extractors';
 
-// console.log((new Marble('/home/tpayen/.mame/hi')).scores);
-// (new Trackfld('/home/tpayen/.mame/nvram')).scores;
-console.log((new Galaga('/home/tpayen/.mame/hi')).scores);
+export default class MameHiExtractor {
+    protected dir = '/home/tpayen/.mame';
+
+    public get(romName: string) {
+        if (extractors[romName]) {
+            console.log(extractors[romName].init(this.dir).scores);
+        }
+    }
+}
