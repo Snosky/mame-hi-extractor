9c7fc9c69355393f7fc44be119575c6d7e0e878e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-09 15:53:36 +0200

    Remove console log

--- src/Extractor/Qbert.ts
+++ src/Extractor/Qbert.ts
@@ -10,7 +10,6 @@ export default class Qbert extends AbstractExtractor {
     extract(): any {
         let currentByte = 2562;
         for (let i = 22; i >= 0; i--) {
-            console.log(this.nvram!.slice(currentByte + 3, 7).byteFilter(0x24).nibbleSkip().buffer);
             this.scores.default.push({
                 rank: i + 1,
                 name: this.nvram!.slice(currentByte, 3).toString({}, 55),
