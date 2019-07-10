6be6da7ee8baee443c24c2382cf803f5594d383e
Tristan Payen <tristan.payen@infopro-digital.com>
2019-07-10 11:37:31 +0200

    Doc

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,20 @@ export default class MameHiExtractor {
         this.dir = dir;
     }
 
+    /**
+     * Return hiscores of a game
+     * @param romName
+     */
     public get(romName: string): Output|undefined {
         if (extractors[romName]) {
             return extractors[romName].init(this.dir).scores;
         }
     }
 
+    /**
+     * Check if hiscore extractor for a game exist
+     * @param romName
+     */
     public exist(romName: string) {
         return !!extractors[romName];
     }
