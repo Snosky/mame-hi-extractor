5b3a86bf895023acca3b97b5ccc959bc65939921
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-04 16:24:38 +0200

    dd outzone extractor

---
+++ src/Extractor/Outzone.ts
@@ -0,0 +1,28 @@
+import Extractor from "../Decorator/Extractor";
+import AbstractExtractor from "../AbstractExtractor";
+
+@Extractor({
+    name: 'outzone'
+})
+export default class Outzone extends AbstractExtractor {
+    protected charset = {
+        0x00: ' '
+    };
+
+    extract(): any {
+        let currentByte = 4;
+        for (let i = 0; i < 5; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).toHexNumber(),
+                name: ''
+            });
+            currentByte += 4
+        }
+        for (let i = 0; i < 5; i++) {
+            this.output.default[i].name = this.hi!.slice(currentByte, 12)
+                .byteMask('FF000000FF000000FF000000').toString(this.charset);
+            currentByte += 16;
+        }
+    }
+}
