1b0c6ed48d6bfccf6fe55a964a77df8eb4732b44
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-11 13:30:27 +0200

    Build

--- dist/Extractor/Hyperspt.d.ts
+++ dist/Extractor/Hyperspt.d.ts
@@ -3,5 +3,5 @@ export default class Hyperspt extends AbstractExtractor {
     protected extraIds: string[];
     extract(): any;
     protected formatScore(score: number, id: string): number;
-    protected getScoreSuffix(id: string): "" | "sec" | "m" | "kg";
+    protected getScoreSuffix(id: string): "" | "sec" | "kg" | "m";
 }
---
+++ dist/Extractor/Snowbros.d.ts
@@ -0,0 +1,4 @@
+import AbstractExtractor from "../AbstractExtractor";
+export default class Snowbros extends AbstractExtractor {
+    extract(): any;
+}
