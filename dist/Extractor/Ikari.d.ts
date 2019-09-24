430eedec916bfc9cedaf8e9e371af4d2240c6ad9
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-24 13:42:53 +0200

    Build

---
+++ dist/Extractor/Ikari.d.ts
@@ -0,0 +1,4 @@
+import AbstractExtractor from "../AbstractExtractor";
+export default class Ikari extends AbstractExtractor {
+    extract(): any;
+}
---
+++ dist/Extractor/Mercs.d.ts
@@ -0,0 +1,4 @@
+import AbstractExtractor from "../AbstractExtractor";
+export default class Mercs extends AbstractExtractor {
+    extract(): any;
+}
