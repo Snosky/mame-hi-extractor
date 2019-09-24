2c7994b22f38db2106757ffc77fe4c8199728fb8
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-24 13:41:16 +0200

    Add avsp extractor

---
+++ src/Extractor/Avsp.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'avsp'
+})
+export default class Avsp extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 50; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte + 4, 4).toHexNumber(),
+                name: this.hi!.slice(currentByte + 8, 3).toString()
+            });
+            currentByte += 16;
+        }
+    }
+}
