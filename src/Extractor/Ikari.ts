ec5d2104450a5f90b8e9a5f832c50d7926461f9c
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-24 13:32:57 +0200

    ADd ikari extractor

---
+++ src/Extractor/Ikari.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'ikari'
+})
+export default class Ikari extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).reverse().toHexNumber() * 10,
+                name: this.hi!.slice(currentByte + 3, 5).toString()
+            });
+            currentByte += 8;
+        }
+    }
+}
