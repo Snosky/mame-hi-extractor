c9aa11490cc7837958036d45de48a567b1fbe7a5
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-24 18:39:45 +0200

    Add extractor for 10yard

---
+++ src/Extractor/10yard.ts
@@ -0,0 +1,25 @@
+import AbstractExtractor from '../AbstractExtractor';
+import Extractor from '../Decorator/Extractor';
+
+@Extractor({
+    name: '10yard',
+    hi: true
+})
+export default class Extractor10yard extends AbstractExtractor {
+    protected charset = {
+        0x5B: '.',
+        0x5C: ' ',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 23; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).reverse().toHexNumber(),
+                name: this.hi!.slice(currentByte + 3, 3).toString(this.charset)
+            });
+            currentByte += 6;
+        }
+    }
+}
