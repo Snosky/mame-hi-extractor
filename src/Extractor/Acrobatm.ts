9146e69b9d5ee2cba96309b56cfcbf55fa4d96bb
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-25 18:28:00 +0200

    Add extractor for acrobatm

---
+++ src/Extractor/Acrobatm.ts
@@ -0,0 +1,26 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'acrobatm',
+})
+export default class Acrobatm extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x2E: '.',
+    };
+
+    extract(): any {
+        let scoreCurrentByte = 0;
+        let nameCurrentByte = 42;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(scoreCurrentByte, 4).toHexNumber(),
+                name: this.hi!.slice(nameCurrentByte, 6).byteSkip(false).toString(this.charset)
+            });
+            scoreCurrentByte += 4;
+            nameCurrentByte += 8;
+        }
+    }
+}
