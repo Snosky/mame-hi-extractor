57b568686c69bea0c3c9104e3df670d61bb6fb1c
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-25 18:05:37 +0200

    Extractor for 4dwarrio

---
+++ src/Extractor/4dwarrio.ts
@@ -0,0 +1,25 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: '4dwarrio'
+})
+export default class Extractor4dwarrio extends AbstractExtractor {
+    protected charset = {
+        0x21: ' ',
+        0x29: ' ',
+        0x6E: '/',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).reverse().toHexNumber(),
+                name: this.hi!.slice(currentByte + 3, 3).toString(this.charset)
+            });
+            currentByte += 16;
+        }
+    }
+}
