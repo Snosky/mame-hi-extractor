a953126c37150694836550a4ee08baacde1ce8ee
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-25 13:38:49 +0200

    Add extractor for batman

---
+++ src/Extractor/Batman.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'batman'
+})
+export default class Batman extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 16; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).readIntBE(),
+                name: this.hi!.slice(currentByte + 3, 2).decodeBase32(true)
+            });
+            currentByte += 5;
+        }
+    }
+}
