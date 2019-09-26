4619e43f192e4e94bb3a2ad537a72bb3c6a0145a
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 11:30:53 +0200

    Add extractor for actionhw

---
+++ src/Extractor/Actionhw.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'actionhw'
+})
+export default class Actionhw extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 5; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                name: this.hi!.slice(currentByte, 3).toString(),
+                score: this.hi!.slice(currentByte + 4, 2).readIntBE() * 100
+            });
+            currentByte += 8;
+        }
+    }
+}
