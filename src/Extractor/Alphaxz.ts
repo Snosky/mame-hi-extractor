0f1379b265aae80edfe80ab4f15eb6dc26be2566
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 16:26:09 +0200

    Add extractor for alphaxz

---
+++ src/Extractor/Alphaxz.ts
@@ -0,0 +1,25 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'alphaxz'
+})
+export default class Alphaxz extends AbstractExtractor {
+    protected charset = {
+        0x2A: ' '
+    };
+
+    extract(): any {
+        let scoreCurrentByte = 0;
+        let nameCurrentByte = 30;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(scoreCurrentByte, 3).toHexNumber() * 10,
+                name: this.hi!.slice(nameCurrentByte, 3).toString(this.charset),
+            });
+            scoreCurrentByte += 3;
+            nameCurrentByte += 10;
+        }
+    }
+}
