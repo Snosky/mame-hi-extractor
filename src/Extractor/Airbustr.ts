b8b717651dc0e94e69ac7c79d9f7bc55d73f351a
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 15:28:28 +0200

    Add extractor for airbustr

---
+++ src/Extractor/Airbustr.ts
@@ -0,0 +1,24 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'airbustr',
+})
+export default class Airbustr extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x40: '@',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 8; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).toHexNumber() * 10,
+                name: this.hi!.slice(currentByte + 3, 4).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
