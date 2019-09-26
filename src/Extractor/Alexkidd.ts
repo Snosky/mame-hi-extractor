f9a62ee19f4fd716be9ebedf8c95aea47e9b2793
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 15:55:27 +0200

    Add extractor for alexkidd

---
+++ src/Extractor/Alexkidd.ts
@@ -0,0 +1,27 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'alexkidd'
+})
+export default class Alexkidd extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x3E: '-',
+        0x5B: '.',
+        0x5E: '!',
+        0x5F: '&',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 7; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).toHexNumber(),
+                name: this.hi!.slice(currentByte + 5, 3).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
