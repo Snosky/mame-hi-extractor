f30dc507fa6113b52aba47082349be7906c79fc4
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 16:01:40 +0200

    Add extractor for alien3

---
+++ src/Extractor/Alien3.ts
@@ -0,0 +1,28 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'alien3'
+})
+export default class Alien3 extends AbstractExtractor {
+    protected charset = {
+        0x22: '…',
+        0x27: '·',
+        0x5B: ' ',
+        0x5C: ' ',
+        0x5D: ' ',
+        0x5E: ' ',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 15; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).readIntLE(),
+                name: this.hi!.slice(currentByte + 4, 3).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
