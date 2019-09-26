cecd5ec60d3c2d684db2e86d3b6f5a07d8415cad
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 16:37:56 +0200

    Add extractor for angelkds

---
+++ src/Extractor/Angelkds.ts
@@ -0,0 +1,26 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'angelkds'
+})
+export default class Angelkds extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x3A: ' ',
+        0x3B: ' ',
+        0x3C: ' ',
+    };
+
+    extract(): any {
+        let currentByte = 3;
+        for (let i = 0; i < 20; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).toHexNumber(),
+                name: this.hi!.slice(currentByte + 4, 10).toString(this.charset)
+            });
+            currentByte += 14;
+        }
+    }
+}
