205ae64706da46b82b078be4049f88e8befafd33
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 16:41:53 +0200

    Add extractor for anteater

---
+++ src/Extractor/Anteater.ts
@@ -0,0 +1,22 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'anteater'
+})
+export default class Anteater extends AbstractExtractor {
+    protected charset = {
+        0x40: ' '
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).toHexNumber(),
+                name: this.hi!.slice(currentByte + 3, 3).toString(this.charset)
+            })
+        }
+    }
+}
