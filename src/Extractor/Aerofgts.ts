f9d523a3742ca28988c216703f911520b3a53ec8
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 12:08:50 +0200

    Add extractor for aerofgts

---
+++ src/Extractor/Aerofgts.ts
@@ -0,0 +1,21 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'aerofgts',
+    hi: false,
+    nvram: 'at28c16'
+})
+export default class Aerofgts extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 8;
+        for (let i = 0; i < 5; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.nvram!.slice(currentByte, 4).readIntLE(),
+                name: this.nvram!.slice(currentByte + 4, 3).toString()
+            });
+            currentByte += 12;
+        }
+    }
+}
