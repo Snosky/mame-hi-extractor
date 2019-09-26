41f9d703cb7e202ed309a450294d729b0d287bf3
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-26 15:11:43 +0200

    Add afighter extractor

---
+++ src/Extractor/Afighter.ts
@@ -0,0 +1,25 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'afighter',
+})
+export default class Afighter extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        0x5C: '?',
+        0x5D: '.',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).toHexNumber(),
+                name: this.hi!.slice(currentByte + 5, 3).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
