7b537c4ec9961bbaff8282a960d660136b721c57
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-27 12:29:58 +0200

    Add extractor for alpham2

---
+++ src/Extractor/Alpham2.ts
@@ -0,0 +1,24 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'alpham2',
+    hi: false,
+    nvram: 'saveram'
+})
+export default class Alpham2 extends AbstractExtractor {
+    extract(): any {
+        this.nvram = this.nvram!.byteSwap(2);
+        let scoreCurrentByte = 904;
+        let nameCurrentByte = 944;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.nvram.slice(scoreCurrentByte, 4).toHexNumber(),
+                name: this.nvram.slice(nameCurrentByte, 3).toString()
+            });
+            scoreCurrentByte += 4;
+            nameCurrentByte += 4;
+        }
+    }
+}
