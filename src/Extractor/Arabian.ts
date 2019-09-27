33b152a6ab8656149d11482f0b07666a779d0f80
Tristan Payen <tristan.payen@infopro-digital.com>
2019-09-27 17:47:56 +0200

    Add extractor for arabian

---
+++ src/Extractor/Arabian.ts
@@ -0,0 +1,23 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'arabian'
+})
+export default class Arabian extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                name: this.hi!.slice(currentByte, 3).toString(this.charset, 55),
+                score: this.hi!.slice(currentByte + 3, 3).toHexNumber()
+            });
+            currentByte += 6;
+        }
+    }
+}
