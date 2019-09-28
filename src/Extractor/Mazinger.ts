00648d4c691f8962e96fcc58b563ab96c5f1895a
Bruno <afronob@gmail.com>
2019-09-29 01:31:06 +0200

    Hiscores bruno (#2)
    * Add New Game Bank Panic Hiscores: bankp
    
    * Add Bankp.ts to Extractor List
    
    * FIX Charset PB and replace it by a space
    
    * Add Charset 0x3d for dot
    
    * Add Mazinger Extractor
    
    * Add Mazinger to Extractor List

---
+++ src/Extractor/Mazinger.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'mazinger'
+})
+export default class Mazinger extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 4;
+        for (let i = 0; i < 8; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).toHexNumber(),
+                name: this.hi!.slice(currentByte + 4, 3).toString()
+            });
+            currentByte += 8;
+        }
+    }
+}
