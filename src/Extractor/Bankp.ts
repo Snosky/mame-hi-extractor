f36c581e341edf4099f339195a3bcc51bf8f6f06
afronob <afronob@gmail.com>
2019-09-28 11:07:28 +0200

    Add New Game Bank Panic Hiscores: bankp

---
+++ src/Extractor/Bankp.ts
@@ -0,0 +1,19 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'bankp'
+})
+export default class Bankp extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 7).nibbleSkip().toHexNumber(),
+                name: this.hi!.slice(currentByte + 7, 3).toString()
+            });
+            currentByte += 16;
+        }
+    }
+}
