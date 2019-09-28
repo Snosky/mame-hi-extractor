8cd18ae290f84e720e1593d15e461b6e2c5587f6
afronob <afronob@gmail.com>
2019-09-28 11:31:48 +0200

    FIX Charset PB and replace it by a space

--- src/Extractor/Bankp.ts
+++ src/Extractor/Bankp.ts
@@ -5,13 +5,16 @@ import Extractor from "../Decorator/Extractor";
     name: 'bankp'
 })
 export default class Bankp extends AbstractExtractor {
+protected charset = {
+        0x00: ' '
+    };
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 10; i++) {
             this.output.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(currentByte, 7).nibbleSkip().toHexNumber(),
-                name: this.hi!.slice(currentByte + 7, 3).toString()
+                name: this.hi!.slice(currentByte + 7, 3).toString(this.charset)
             });
             currentByte += 16;
         }
