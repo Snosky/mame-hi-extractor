496211e3c265b39135a2877c7a521a0e380e79e6
afronob <afronob@gmail.com>
2019-09-29 16:00:49 +0200

    Add Digdug 2 Extractor

---
+++ src/Extractor/Digdug2.ts
@@ -0,0 +1,22 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'digdug2'
+})
+export default class Digdug2 extends AbstractExtractor {
+protected charset = {
+        0x5B: '.'
+    };
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 5; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 6).nibbleSkip().toHexNumber() *10,
+                name: this.hi!.slice(currentByte + 6, 6).toString(this.charset)
+            });
+            currentByte += 16;
+        }
+    }
+}
