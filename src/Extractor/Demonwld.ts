5def5fe6cda044001cba13acf3fa16dd83436d2d
afronob <afronob@gmail.com>
2019-09-30 17:20:31 +0200

    FACTO one loop for rule them all (demonwld)

--- src/Extractor/Demonwld.ts
+++ src/Extractor/Demonwld.ts
@@ -6,23 +6,19 @@ import Extractor from "../Decorator/Extractor";
 })
 export default class Demonwld extends AbstractExtractor {
 protected charset = {
-        0x5B: '&black-heart;'
+        0x5B: ' '
     };
     extract(): any {
-        let currentByte = 0;
+        let scoreCurrentByte = 0;
+        let nameCurrentByte = 80;
         for (let i = 0; i < 20; i++) {
             this.output.default.push({
                 rank: i + 1,
-                score: this.hi!.slice(currentByte, 4).toHexNumber(),
-                name: '---'
+                score: this.hi!.slice(scoreCurrentByte, 4).toHexNumber(),
+                name: this.hi!.slice(nameCurrentByte, 6).byteSkip(false).toString(this.charset)
             });
-            currentByte += 4;
+            scoreCurrentByte += 4;
+            nameCurrentByte += 6;
         }
-        currentByte = 80;
-        for (let i = 0; i < 20; i++) {
-            this.output.default[i].name = this.hi!.slice(currentByte, 6).byteSkip(false).toString(this.charset);
-            currentByte += 6;
-        }
-
     }
 }
