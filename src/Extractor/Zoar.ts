a0dfffff7642ef0da06448a60fa00cf6920a4518
afronob <afronob@gmail.com>
2019-09-30 17:44:55 +0200

    FIX Changing Fetch Score Method

--- src/Extractor/Zoar.ts
+++ src/Extractor/Zoar.ts
@@ -11,7 +11,7 @@ export default class Zoar extends AbstractExtractor {
         for (let i = 0; i < 5; i++) {
             this.output.default.push({
                 rank: i + 1,
-                score: this.hi!.slice(scoreCurrentByte, 3).toHexNumber(),
+                score: this.hi!.slice(scoreCurrentByte, 3).reverse().toHexNumber(),
                 name: this.hi!.slice(nameCurrentByte, 3).toString()
             });
             scoreCurrentByte += 3;
