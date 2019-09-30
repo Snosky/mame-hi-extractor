a4cecc5ac8a6226b487c19c4b1db888932a788b6
Bruno <afronob@gmail.com>
2019-09-30 17:53:41 +0200

    Hiscores bruno (#3)
    * Add New Game Bank Panic Hiscores: bankp
    
    * Add Bankp.ts to Extractor List
    
    * FIX Charset PB and replace it by a space
    
    * Add Charset 0x3d for dot
    
    * Add Mazinger Extractor
    
    * Add Mazinger to Extractor List
    
    * Add Zoar Extractor
    
    * Add "Dragon Breed" Extractor
    
    * Register Dbreed Extractor
    
    * Add Demon's World Extractor
    
    * Register Demonwld Extractor
    
    * Add Digdug 2 Extractor
    
    * Register Digdug2 Extractor
    
    * Add Docastle Extractor
    
    * Register Docastle Extractor
    
    * Add Do! Run Run Extractor
    
    * Register Dorunrun Extractor
    
    * FACTO one loop for rule them all (zoar)
    
    * FACTO one loop for rule them all (demonwld)
    
    * FIX Black-Heart Cannot Exist has htmlentity (dorunrun)
    
    * REFACTO Changing Fetch Score Method
    
    * FIX Changing Fetch Score Method

---
+++ src/Extractor/Docastle.ts
@@ -0,0 +1,22 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'docastle'
+})
+export default class Docastle extends AbstractExtractor {
+protected charset = {
+        0x5B: '.'
+    };
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).toHexNumber(),
+                name: this.hi!.slice(currentByte + 3, 3).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
