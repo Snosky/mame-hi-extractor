062487ca167a09eb73ffc37844e1226fcf6bfe5f
Tristan Payen <tristan.payen@infopro-digital.com>
2019-10-02 17:58:10 +0200

    Add extractor for arknoid2

---
+++ src/Extractor/Arknoid2.ts
@@ -0,0 +1,20 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'arknoid2'
+})
+export default class Arknoid2 extends AbstractExtractor {
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 5; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 3).toHexNumber() * 10,
+                round: this.hi!.slice(currentByte + 3, 1).readIntBE(),
+                name: this.hi!.slice(currentByte + 4, 3).toString()
+            });
+            currentByte += 8;
+        }
+    }
+}
