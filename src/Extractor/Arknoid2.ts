f5b49758f75d7e1f7ac1cab3d3f3cea0a76e9ccd
Tristan Payen <tristan.payen@infopro-digital.com>
2019-10-02 18:12:22 +0200

    Convert round to string

--- src/Extractor/Arknoid2.ts
+++ src/Extractor/Arknoid2.ts
@@ -5,13 +5,14 @@ import Extractor from "../Decorator/Extractor";
     name: 'arknoid2'
 })
 export default class Arknoid2 extends AbstractExtractor {
+
     extract(): any {
         let currentByte = 0;
         for (let i = 0; i < 5; i++) {
             this.output.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(currentByte, 3).toHexNumber() * 10,
-                round: this.hi!.slice(currentByte + 3, 1).readIntBE(),
+                round: this.hi!.slice(currentByte + 3, 1).readIntBE().toString(),
                 name: this.hi!.slice(currentByte + 4, 3).toString()
             });
             currentByte += 8;
