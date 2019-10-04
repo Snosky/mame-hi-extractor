5f847814d3baca6842fb0e2cd706ec9080ca4681
Tristan Payen <tristan.payen@infopro-digital.com>
2019-10-04 12:05:50 +0200

    Add extractor for armedf

---
+++ src/Extractor/Armedf.ts
@@ -0,0 +1,26 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'armedf'
+})
+export default class Armedf extends AbstractExtractor {
+    protected charset = {
+        0x5B: ' ',
+        0x5C: ' ',
+        0x5D: '&',
+        0x5E: '.'
+    };
+
+    extract(): any {
+        let currentByte = 0;
+        for (let i = 0; i < 10; i++) {
+            this.output.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 4).toHexNumber(),
+                name: this.hi!.slice(currentByte + 4, 3).toString(this.charset)
+            });
+            currentByte += 8;
+        }
+    }
+}
