327b8ffede0343e447d4dd2070bd154055088820
afronob <afronob@gmail.com>
2019-10-08 19:39:07 +0200

    Complete charset values. Comment lines about character & special for future implementation

--- src/Extractor/Nemesis.ts
+++ src/Extractor/Nemesis.ts
@@ -6,26 +6,28 @@ import Extractor from "../Decorator/Extractor";
 })
 export default class Nemesis extends AbstractExtractor {
 protected charset = {
+        0x00: ' ',
+        0x1B: '-',
         0x1C: '.'
     };
-protected character = {
-        0x00: 'MALE',
-        0x01: 'FEMALE'
-    };
-protected special = {
-        0x0A: 'Aquarius',
-        0x00: 'Aries',
-        0x01: 'Taurus',
-        0x02: 'Gemini',
-        0x03: 'Cancer',
-        0x04: 'Leo',
-        0x05: 'Virgo',
-        0x06: 'Libra',
-        0x07: 'Scorpio',
-        0x08: 'Sagittarius',
-        0x09: 'Capricorn',
-        0x11: 'Pices'
-    };
+// protected character = {
+//         0x00: 'MALE',
+//         0x01: 'FEMALE'
+//     };
+// protected special = {
+//         0x0A: 'Aquarius',
+//         0x00: 'Aries',
+//         0x01: 'Taurus',
+//         0x02: 'Gemini',
+//         0x03: 'Cancer',
+//         0x04: 'Leo',
+//         0x05: 'Virgo',
+//         0x06: 'Libra',
+//         0x07: 'Scorpio',
+//         0x08: 'Sagittarius',
+//         0x09: 'Capricorn',
+//         0x11: 'Pices'
+//     };
     extract(): any {
         let scoreCurrentByte = 1;
         let nameCurrentByte = 3;
@@ -33,8 +35,8 @@ protected special = {
             this.output.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(scoreCurrentByte +10, 4).toHexNumber(),
-                character: this.hi!.slice(scoreCurrentByte, 1).toString(this.character),
-                special: this.hi!.slice(scoreCurrentByte + 1, 1).toString(this.special),
+                // character: this.hi!.slice(scoreCurrentByte, 1).toString(this.character),
+                // special: this.hi!.slice(scoreCurrentByte + 1, 1).toString(this.special),
                 name: this.hi!.slice(nameCurrentByte, 3).toString(this.charset,64)
             });
             scoreCurrentByte += 14;
