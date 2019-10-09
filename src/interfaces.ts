3f9b053792d5aa379b2d3fef0fc100df573491ea
Tristan Payen <tristan.payen@infopro-digital.com>
2019-10-09 12:47:18 +0200

    WIP

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -8,12 +8,26 @@ export interface Score {
     score: number;
     name: string;
     scoreSuffix?: string;
+    extra?: any
+}
+
+export interface ScoreExtra {
     round?: string;
-    // character?: number; Character ID
+    character?: number; // Character ID
+    stage?: number;
 }
 
 export interface ExtractorOptions {
     name: string,
     hi?: boolean,
     nvram?: string;
+    data?: ExtractorOptionsData;
+}
+
+export interface ExtractorOptionsData {
+    characters?: ExtractorOptionsDataCharacters;
+}
+
+export interface ExtractorOptionsDataCharacters {
+    [key: number]: string;
 }
