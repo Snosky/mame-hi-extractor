6c8718298cd3a268b852440dec85511a908a873a
Tristan Payen <tristan.payen@infopro-digital.com>
2022-06-17 16:23:42 +0200

    fix: airattck get 8 scores instead of 5

--- src/Extractor/airattck.ts
+++ src/Extractor/airattck.ts
@@ -11,7 +11,7 @@ export default class Airattck extends AbstractExtractor {
 
     extract(): this {
         let currentByte = 0;
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 8; i++) {
             this.output.default.push({
                 rank: i + 1,
                 score: this.hi!.slice(currentByte, 4).toHexNumber(),
