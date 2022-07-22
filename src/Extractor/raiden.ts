922b9f44737b84a6dcb3a5726293dc436ebb060c
Bruno <afronob@gmail.com>
2022-07-22 11:00:39 +0200

    Hiscores raiden (#25)
    * Add Raiden Test file
    
    * Dual Scores for Raiden Added
    
    * add raiden screenshots
    
    Co-authored-by: Bruno TEFFOT <bteffot@infopro-digital.com>
    Co-authored-by: Tristan Payen <tristan.payen@infopro-digital.com>

---
+++ src/Extractor/raiden.ts
@@ -0,0 +1,39 @@
+import AbstractExtractor from "../AbstractExtractor";
+import Extractor from "../Decorator/Extractor";
+
+@Extractor({
+    name: 'raiden'
+})
+export default class Raiden extends AbstractExtractor {
+    protected charset = {
+        0x00: ' ',
+        /*0x2e: '.',*/
+        0x5b: '!',
+        0x5c: '?',
+        0x5d: '-'
+};
+
+extract(): this {
+        let currentByte = 0;
+        for (let i = 0; i < 9; i++) {
+            this.scores.default.push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 2).byteSwap(2).buffer.readUInt16BE(),
+                name: this.hi!.slice(currentByte + 6, 3).toString(this.charset)
+            });
+            currentByte += 10;
+        }
+
+        this.output.extras = {'dual': []};
+        for (let i = 0; i < 9; i++) {
+            this.output.extras['dual'].push({
+                rank: i + 1,
+                score: this.hi!.slice(currentByte, 2).byteSwap(2).buffer.readUInt16BE(),
+                name: this.hi!.slice(currentByte + 6, 3).toString(this.charset)
+            });
+            currentByte += 10;
+        }
+
+        return this;
+    }
+}
